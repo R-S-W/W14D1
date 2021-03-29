90fb75b8dccd81f829b1d989805b6e963d368273
Ray <raysidarta@gmail.com>
2021-03-29 09:50:51 -0400

    work on frontend and setup

--- app.js
+++ app.js
@@ -2,11 +2,44 @@ const express = require("express");
 const mongoose = require('mongoose');
 const app  = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const User = require('./models/User');
+
+const passport = require('passport')
+
 mongoose 
   .connect(db, {useNewUrlParser: true})
   .then(()=> console.log('Connected to MongoDB successfully'))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!!!"));
+app.use(bodyParser.urlencoded({//app responds to other software like postman
+  extended: false
+}));
+app.use(bodyParser.json());// respond to json requests
+
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
+
+
+// app.get("/", (req, res) => {
+//   const user = new User({
+//     handle: 'jim',
+//     email: 'jim@jim.jim',
+//     password: 'jimisgreat123'
+//   })
+//   user.save();
+//   res.send("Hello World!!!");
+// });
+
+
+app.use("/api/users", users);
+app.use('/api/tweets', tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 const port  = process.env.PORT || 5000;
 app.listen(port, ()=>console.log(`Server is running on port ${port}`));
---
+++ frontend/src/components/main_page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>
+          <footer>
+            Copyright  &copy; 2019 Chirper
+          </footer>
+        </h1>
+      </div>
+    )
+  }
+}
+export default MainPage;
